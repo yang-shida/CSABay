a821efd39400bc339574224006312c031969a142
shida-yang <32109151+shida-yang@users.noreply.github.com>
2021-02-14 20:02:40 -0500

    signup page complete

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # CSABay
 A website Craigslist/eBay type website where people can publicize selling information and contact information for sellers
+
+## Dependencies:
+- antd
+- antd-password-input-strength
+- JSON Server (used as fake DB)
--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -12,6 +12,7 @@ import {
   AutoComplete,
 } from 'antd';
 import { QuestionCircleOutlined } from '@ant-design/icons';
+import {PasswordInput} from 'antd-password-input-strength'
 
 const formItemLayout = {
     labelCol: {
@@ -44,17 +45,53 @@ const tailFormItemLayout = {
 }
 
 const SignupForm = () => {
+    const [form] = Form.useForm();
 
-    const onFinish = () =>{
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [wechatID, setWechatID] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirm, setConfirm] = useState('')
+    const [phoneNum, setPhoneNum] = useState('')
 
+
+    const onFinish = async () =>{
+        const newUser = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            wechatID: wechatID,
+            password: password,
+            phoneNum: phoneNum
+        }
+
+    const res = await fetch('http://localhost:8080/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newUser),
+    })
+
+        setFirstName('')
+        setLastName('')
+        setEmail('')
+        setWechatID('')
+        setPassword('')
+        setConfirm('')
+        setPhoneNum('')
+
+        form.resetFields();
+
+        // Go to login page
     }
-
-
 
     return (
         <div>
             <h1>Register</h1>
             <Form
+                form={form}
                 {...formItemLayout}
                 name="signup"
                 onFinish={onFinish}
@@ -71,7 +108,7 @@ const SignupForm = () => {
                     ]
                     }
                 >
-                    <Input />
+                    <Input value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
                 </Form.Item>
 
                 <Form.Item
@@ -85,7 +122,7 @@ const SignupForm = () => {
                     ]
                     }
                 >
-                    <Input />
+                    <Input value={lastName} onChange={(e) => setLastName(e.target.value)}/>
                 </Form.Item>
 
                 <Form.Item
@@ -102,43 +139,84 @@ const SignupForm = () => {
                         },
                     ]}
                 >
-                    <Input />
+                    <Input value={email} onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Item>
 
                 <Form.Item
                     name="wechat-id"
                     label="WeChat ID"
                 >
-                    <Input />
+                    <Input value={wechatID} onChange={(e) => setWechatID(e.target.value)}/>
                 </Form.Item>
 
-                <Form.Item
+                <Form.Item 
+                    label={
+                        <span>
+                            Password&nbsp;
+                            <Tooltip title="Password should be at least 6 characters long. You should include numbers, upper-case letters, and lower-case letters in your password.">
+                            <QuestionCircleOutlined />
+                            </Tooltip>
+                        </span>
+                    }
                     name="password"
-                    label="Password"
                     rules={[
                         {
                             required: true,
                             message: 'Please input your password!',
                         },
-                    ]}
+                        () => ({
+                            validator(_, value) {
+                                let hasNum = /\d/.test(value);
+                                let hasUpper = /[A-Z]/.test(value);
+                                let hasLower = /[a-z]/.test(value);
+                                let hasSpace = / /.test(value)
+                                if(hasSpace){
+                                    return Promise.reject('Password cannot contain space!');
+                                }
+                                else if (value.length < 6) {
+                                    return Promise.reject('Password should be at least 6 characters long!');
+                                }
+                                else if(!hasNum || !hasUpper || !hasLower){
+                                    return Promise.reject('Please include numbers, upper-case letters, lower-case letters in your password!');
+                                }
+                                else{
+                                    return Promise.resolve();
+                                }
+                                
+                            },
+                        }),
+                    ]}    
                     hasFeedback
                 >
-                    <Input.Password />
+                    <PasswordInput 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        settings={{
+                            colorScheme: {
+                                levels: ["#ff4033", "#fe940d", "#ffd908", "#cbe11d", "#6ecc3a"],
+                                noLevel: "lightgrey"
+                            },
+                            height: 7,
+                            alwaysVisible: true
+                        }}
+                        inputProps={{
+                            size: 'medium'
+                        }}
+                    />
                 </Form.Item>
 
                 <Form.Item
                     name="confirm"
                     label="Confirm Password"
-                    dependencies={['password']}
                     hasFeedback
                     rules={[
                         {
                             required: true,
                             message: 'Please confirm your password!',
                         },
-                        ({ getFieldValue }) => ({
+                        () => ({
                             validator(_, value) {
-                            if (!value || getFieldValue('password') === value) {
+                            if (!value || password === value) {
                                 return Promise.resolve();
                             }
 
@@ -147,45 +225,14 @@ const SignupForm = () => {
                         }),
                     ]}
                 >
-                    <Input.Password />
-                </Form.Item>
-
-                <Form.Item
-                    name="nickname"
-                    label={
-                    <span>
-                        Nickname&nbsp;
-                        <Tooltip title="What do you want others to call you?">
-                        <QuestionCircleOutlined />
-                        </Tooltip>
-                    </span>
-                    }
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your nickname!',
-                            whitespace: true,
-                        },
-                    ]}
-                >
-                    <Input />
+                    <Input.Password value={confirm} onChange={(e) => setConfirm(e.target.value)}/>
                 </Form.Item>
 
                 <Form.Item
                     name="phone"
                     label="Phone Number"
-                    rules={[
-                    {
-                        required: true,
-                        message: 'Please input your phone number!',
-                    },
-                    ]}
                 >
-                    <Input
-                    style={{
-                        width: '100%',
-                    }}
-                    />
+                    <Input value={phoneNum} onChange={(e) => setPhoneNum(e.target.value)}/>
                 </Form.Item>
 
                 <Form.Item {...tailFormItemLayout}>
