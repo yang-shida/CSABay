576059ef213e250522dd7e7b91c11ee8a40802a4
shida-yang <32109151+shida-yang@users.noreply.github.com>
2021-04-18 18:54:33 -0400

    comment out get-user-info-by-id

--- routes/user_route.js
+++ routes/user_route.js
@@ -208,49 +208,50 @@ router.route("/get-user-info").get(
     }
 )
 
-router.route("/get-user-info-by-id").get(
-    (request, response) => {
-        const userID = request.body.userID
-        if(!userID){
-            return response.json(
-                {
-                    code: 1,
-                    message: "Did not receive UserID!"
-                }
-            )
-        }
+// Not used in current implementation
+// router.route("/get-user-info-by-id").get(
+//     (request, response) => {
+//         const userID = request.body.userID
+//         if(!userID){
+//             return response.json(
+//                 {
+//                     code: 1,
+//                     message: "Did not receive UserID!"
+//                 }
+//             )
+//         }
 
-        User.findOne({_id: userID}, hidePwdAndID).exec(
-            (err, doc) => {
-                if(err){
-                    console.log(err)
-                    return response.json(
-                        {
-                            code: 1,
-                            message: "Something went wrong on our end."
-                        }
-                    )
-                }
-                if(!doc){
-                    return response.json(
-                        {
-                            code: 1,
-                            message: "User not found!"
-                        }
-                    )
-                }
-                else{
-                    return response.json(
-                        {
-                            code: 0,
-                            data: doc
-                        }
-                    )
-                }
-            }
-        )
-    }
-)
+//         User.findOne({_id: userID}, hidePwdAndID).exec(
+//             (err, doc) => {
+//                 if(err){
+//                     console.log(err)
+//                     return response.json(
+//                         {
+//                             code: 1,
+//                             message: "Something went wrong on our end."
+//                         }
+//                     )
+//                 }
+//                 if(!doc){
+//                     return response.json(
+//                         {
+//                             code: 1,
+//                             message: "User not found!"
+//                         }
+//                     )
+//                 }
+//                 else{
+//                     return response.json(
+//                         {
+//                             code: 0,
+//                             data: doc
+//                         }
+//                     )
+//                 }
+//             }
+//         )
+//     }
+// )
 
 router.route("/update-user-info").put(
     (request, response) => {
